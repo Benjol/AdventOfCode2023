d5e20654dbc5f5437e6ee915c1588af414d7f476
Benjol <benjol@github.com>
2023-12-11 15:22:07 +0100

    Day8 part2

--- Day8.fsx
+++ Day8.fsx
@@ -15,7 +15,7 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)"
 
-let regex = Regex("[A-Z]{3}")
+let regex = Regex("[A-Z1-2]{3}")
 type Instruction = { Id: string; Left : string; Right : string }
     with
         static member Parse txt =
@@ -35,17 +35,20 @@ type Instructions = { Directions : seq<Direction>; Network : Map<string,Instruct
             let network = arr |> Array.skip 1 |> Array.map Instruction.Parse |> Array.map (fun i -> i.Id, i) |> Map.ofArray
             { Directions = directions; Network = network }
 
-let part1 (input) =
-    let instructions = input |> Instructions.Parse
+let length instructions station endcondition =
     instructions.Directions
         |> Seq.scan (fun (station,steps) direction ->
                             let instruction = Map.find station instructions.Network
                             match direction with
                             | Left -> instruction.Left, steps + 1
-                            | Right -> instruction.Right, steps + 1) ("AAA", 0)
+                            | Right -> instruction.Right, steps + 1) (station, 0)
 
-        |> Seq.find (fun (station, _) -> station = "ZZZ")
+        |> Seq.find (fun (station, _) -> endcondition station)
         |> snd
+
+let part1 (input) =
+    let instructions = input |> Instructions.Parse
+    length instructions "AAA" (fun s -> s = "ZZZ")
 
 let testoutput1 = testinput |> part1
 printfn "Test part 1 output: %A" testoutput1
@@ -63,10 +66,37 @@ let input1 = File.ReadAllText("Day8.txt")
 let output1 = input1 |> part1
 printfn "Part 1 output: %A" output1
 
-//let part2 (games:seq<string>) = ""
+let testinput3 = "LR
 
-//let testoutput2 = testinput |> Split2 '\r' '\n' |> part2
-//printfn "Test part 2 output: %A" testoutput2
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"
 
-//let output2 = input1 |> part2
-//printfn "Part 2 output: %A" output2
+let part2 (input) =
+    let instructions = input |> Instructions.Parse
+    let starts = instructions.Network.Keys |> Seq.filter (fun n -> n.EndsWith("A"))
+
+    let lengths = starts |> Seq.map (fun station -> length instructions station (fun s -> s.EndsWith("Z")))
+
+    let rec gcd x y =
+        if y = 0L then x
+        else gcd y (x % y)
+    let rec lcm2 x y = abs (x * y) / (gcd x y)
+    let rec lcm nums =
+        match nums with
+        | [] -> 0L
+        | [x;y] -> lcm2 x y
+        | x::tail -> lcm2 x (lcm tail)
+    lengths |> Seq.iter (printfn "%A")
+    lengths |> Seq.map int64 |> List.ofSeq |> lcm
+
+let testoutput2 = testinput3 |> part2
+printfn "Test part 2 output: %A" testoutput2
+
+let output2 = input1 |> part2
+printfn "Part 2 output: %A" output2
